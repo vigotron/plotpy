7d62977856f690ff904757f2f4f240c2982d305a
vigotron <sempai88@gmail.com>
2020-05-28 17:56:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo 'Building Plot Script'
-docker build -t plotpy:master .
+docker build -t plotpy:master . /bin/bash
 docker run -d \
 -it \
 --mount type=bind,source="$(pwd)"/,target=/app \
-plotpy:master
+plotpy:master \
+/bin/bash
