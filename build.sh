33e24851c245012806609021c3f6f092c478492e
vigo <victor.gonzalez@brokergenius.com>
2020-05-28 17:54:57 +0200

    update build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-echo 'Building Plot Script'
-docker build -t plotpy:master .
+echo 'Execute build and run of Py Script'
+docker build -t plotpy:master . /bin/bash
 docker run -d \
 -it \
 --mount type=bind,source="$(pwd)"/,target=/app \
 plotpy:master
+/bin/bash
