ded4005e6493dbc3f262e9c4c570858c515c17e6
vigo <victor.gonzalez@brokergenius.com>
2020-05-29 18:59:41 +0200

    add output folder

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 echo 'Execute build and run of Py Script'
-docker build -t plotpy:master . 
+docker build -t plotpy:master .
+mkdir output
 docker run -d -it --mount type=bind,source="$(pwd)"/output,target=/app plotpy:master
