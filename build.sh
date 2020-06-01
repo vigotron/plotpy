7e6515efabbe759a77f0eae6937481901d14df7a
vigo <victor.gonzalez@brokergenius.com>
2020-06-01 13:10:45 +0200

    run output

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 echo 'Execute build and run of Py Script'
 mkdir output
-mkdir imagefs
-docker build -t plotpy:master "$(pwd)"/imagefs
-docker run -d -w imagefs --mount type=bind,src="$(pwd)"/output,dst=/app plotpy:master
+docker build -t plotpy:master "$(pwd)"/output
+docker run -d --mount type=bind,src="$(pwd)"/output,dst=/app plotpy:master
