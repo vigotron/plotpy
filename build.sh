50cf622961464ac5be777a0e8ffb7163df3492e2
vigo <victor.gonzalez@brokergenius.com>
2020-06-01 13:03:53 +0200

    change folders

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo 'Execute build and run of Py Script'
 mkdir output
 mkdir imagefs
-docker build -t plotpy:master /imagefs
+docker build -t plotpy:master "$(pwd)"/imagefs
 docker run -d -w imagefs --mount type=bind,src="$(pwd)"/output,dst=/app plotpy:master
