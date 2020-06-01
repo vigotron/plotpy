9d073cf2bf75ef01b80c6b72dc603e3bdc9ebc69
vigo <victor.gonzalez@brokergenius.com>
2020-06-01 13:50:28 +0200

    copy files to output

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 echo 'Execute build and run of Py Script'
 mkdir output
 docker build -t plotpy:master .
-docker run -d --mount type=bind,src="$(pwd)"/output,dst=/app plotpy:master
+docker run -d --mount type=bind,src="$(pwd)",dst=/app plotpy:master
